40579ea9223416f13f2515bae074b8d4c6359551
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2021-04-25 17:08:45 +0400

    add ProductManager and Category

--- Lesson 2 - OOP Introduction/src/Main.java
+++ Lesson 2 - OOP Introduction/src/Main.java
@@ -1,26 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        // product1
-        Product product1 = new Product();
-        product1.id = 1;
-        product1.name = "Lenovo V14";
-        product1.unitPrice = 15000;
-        product1.detail = "16 GB RAM";
 
-        // product 2
-        Product product2 = new Product();
-        product2.id = 2;
-        product2.name = "Lenovo V15";
-        product2.unitPrice = 16000;
-        product2.detail = "32 GB RAM";
-
-        // product 3
-        Product product3 = new Product();
-        product3.id = 3;
-        product3.name = "HP 5";
-        product3.unitPrice = 10000;
-        product3.detail = "8 GB RAM";
+        Product product1 = new Product(1, "Lenovo V14", 15000, "16 GB RAM");
+        Product product2 = new Product(2, "Lenovo V15", 16000, "32 GB RAM");
+        Product product3 = new Product(3, "HP 5", 10000, "8 GB RAM");
 
         Product[] products = {product1, product2, product3};
 
@@ -29,5 +13,18 @@ public class Main {
         }
 
         System.out.println(products.length);
+
+        Category category1 = new Category();
+        category1.id = 1;
+        category1.name = "Laptop";
+
+        Category category2 = new Category();
+        category2.id = 2;
+        category2.name = "Home/Office";
+
+        ProductManager productManager = new ProductManager();
+        productManager.addToCart(product1);
+        productManager.addToCart(product2);
+        productManager.addToCart(product3);
     }
 }
