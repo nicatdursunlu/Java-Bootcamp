4fc3e1d8fd51c8e34e94484ac62c52bb737876ba
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2021-05-08 14:13:54 +0400

    finish Lesson 04 - Interfaces

--- Lesson 04 - Interfaces/src/Main.java
+++ Lesson 04 - Interfaces/src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Logger[] loggers = { new SmsLogger(), new EmailLogger(), new FileLogger() };
+
+        CustomerManager customerManager = new CustomerManager(loggers);
+        Customer engin = new Customer(1, "Engin", "Demirog");
+        customerManager.add(engin);
     }
 }
