6cec1dc8aed9c2b81e4aaec844ef8e532baed12c
nicatdursunlu <nicat.dursunlu@gmail.com>
2021-05-29 12:27:18 +0400

    finish  Lesson 05 - NLayeredDemo

--- Lesson 05 - NLayeredDemo/src/Main.java
+++ Lesson 05 - NLayeredDemo/src/Main.java
@@ -1,14 +1,14 @@
 import business.abstracts.ProductService;
 import business.concretes.ProductManager;
+import core.JLoggerManagerAdapter;
 import dataAccess.concretes.AbcProductDao;
 import entities.concretes.Product;
-
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ProductService productService = new ProductManager(new AbcProductDao());
+        ProductService productService = new ProductManager(new AbcProductDao(), new JLoggerManagerAdapter());
 
         Product product = new Product(1, 2, "Apple", 12, 50);
         productService.add(product);
--- Lesson 05 - NLayeredDemo/src/business/concretes/ProductManager.java
+++ Lesson 05 - NLayeredDemo/src/business/concretes/ProductManager.java
@@ -1,16 +1,20 @@
 package business.concretes;
 
 import business.abstracts.ProductService;
+import core.LoggerService;
 import dataAccess.abstracts.ProductDao;
 import entities.concretes.Product;
 
 import java.util.List;
 
 public class ProductManager implements ProductService {
-    private ProductDao productDao;
 
-    public ProductManager(ProductDao productDao) {
+    private ProductDao productDao;
+    private LoggerService loggerService;
+
+    public ProductManager(ProductDao productDao, LoggerService loggerService) {
         this.productDao = productDao;
+        this.loggerService = loggerService;
     }
 
     @Override
@@ -21,6 +25,7 @@ public class ProductManager implements ProductService {
         }
 
         this.productDao.add(product);
+        this.loggerService.logToSystem("Product added " + product.getName());
     }
 
     @Override
