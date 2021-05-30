c2a532fc3f409f5331c9b9738e6ad507031c4978
nicatdursunlu <nicat.dursunlu@gmail.com>
2021-05-30 19:09:11 +0400

    update Northwind

--- northwind/northwind/src/main/java/kodlamaio/northwind/core/utilities/results/ErrorResult.java
+++ northwind/northwind/src/main/java/kodlamaio/northwind/core/utilities/results/ErrorResult.java
@@ -3,11 +3,11 @@ package kodlamaio.northwind.core.utilities.results;
 public class ErrorResult extends Result {
 
 	public ErrorResult() {
-			super(false);
-		}
+		super(false);
+	}
 		
-		public ErrorResult(String message) {
-			super(false, message);
-		}
+	public ErrorResult(String message) {
+		super(false, message);
+	}
 	
 }
