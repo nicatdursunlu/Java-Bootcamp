a0dd670711a838080a0983b60438cbd15d3f0529
Nijat Dursunlu <nicat.dursunlu@gmail.com>
2021-06-02 11:20:52 +0400

    update Northwind

--- northwind/northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ northwind/northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -8,6 +8,9 @@ import kodlamaio.northwind.entities.concretes.Product;
 
 public interface ProductService {
 	DataResult<List<Product>> getAll();
+	DataResult<List<Product>> getAllSorted();
+	DataResult<List<Product>> getAll(int pageNo, int pageSize);
+	
 	Result add(Product product);
 	
 	 DataResult<Product> getByProductName(String productName);
@@ -23,5 +26,5 @@ public interface ProductService {
      DataResult<List<Product>> getByProductNameStartsWith(String productName);
 
      DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId);
-     
+  
 }
--- northwind/northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ northwind/northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -3,6 +3,9 @@ package kodlamaio.northwind.business.concretes;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -29,6 +32,21 @@ public class ProductManager implements ProductService {
 	public DataResult<List<Product>> getAll() {
 		return new SuccessDataResult<List<Product>>
 		(this.productDao.findAll(), "Data is listed");
+	}
+	
+	@Override
+	public DataResult<List<Product>> getAllSorted() {
+		Sort sort = Sort.by(Sort.Direction.DESC, "productName");
+		return new SuccessDataResult<List<Product>>
+		(this.productDao.findAll(sort), "Data is listed");
+	}
+	
+	@Override
+	public DataResult<List<Product>> getAll(int pageNo, int pageSize) {
+		Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+		
+		return new SuccessDataResult<List<Product>> 
+		(this.productDao.findAll(pageable).getContent(), "Data is listed");
 	}
 
 	@Override
