115c3f4cb0e58da76938c91a43b0da4ab2fc5a24
Nijat Dursunlu <nicat.dursunlu@gmail.com>
2021-06-04 00:37:00 +0400

    add Homework 03 - Coffee Shop

---
+++ Homeworks/Homework 03 - Coffee Shop/src/abstracts/BaseCustomerManager.java
@@ -0,0 +1,11 @@
+package abstracts;
+
+import entities.Customer;
+
+public abstract class BaseCustomerManager implements CustomerService {
+	
+	@Override
+	public void save(Customer customer) {
+		System.out.println("Saved to do: " + customer.getFirstName());
+	}
+}
